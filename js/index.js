1b101d2a54db2b0e6984de963d2315f21c8aef8b
Faria Afrose <85243808+faria-afrose@users.noreply.github.com>
2023-06-28 23:02:55 +0600

    Update index.js

--- js/index.js
+++ js/index.js
@@ -8,34 +8,34 @@ window.addEventListener("scroll", function () {
 const cookeDiv = document.getElementById("cookieWraps");
 const decline = document.getElementById("decline");
 const accept = document.getElementById("cookieWraps-b2");
-// window.addEventListener("load", function (e) {
-//   setTimeout(() => {
-//     const cookieAccept = document.cookie.indexOf("CookieSetBy=Faria") !== -1;
-//     const cookieDecline = document.cookie.indexOf("CookieSetBy=false") !== -1;
-//     cookeDiv.classList.add("show");
-//   }, 2000);
+window.addEventListener("load", function (e) {
+  setTimeout(() => {
+    const cookieAccept = document.cookie.indexOf("CookieSetBy=Faria") !== -1;
+    const cookieDecline = document.cookie.indexOf("CookieSetBy=false") !== -1;
+    cookeDiv.classList.add("show");
+  }, 2000);
 
-//   accept.addEventListener("click", function (e) {
-//     cookeDiv.classList.remove("show");
-//     cookeDiv.classList.add("hide");
-//     //1min=60s ,1hr=60m,1day=24, 30day=30
-//     //   max-age=" + 60 * 60 * 24 * 30=1month
-//     document.cookie = "CookieSetBy=Faria; max-age=" + 60 * 1000; //1 min
-//   });
-//   decline.addEventListener("click", function () {
-//     document.cookie = "CookieSetBy=false; max-age=" + 60 * 1000; //1 min
+  accept.addEventListener("click", function (e) {
+    cookeDiv.classList.remove("show");
+    cookeDiv.classList.add("hide");
+    //1min=60s ,1hr=60m,1day=24, 30day=30
+    //   max-age=" + 60 * 60 * 24 * 30=1month
+    document.cookie = "CookieSetBy=Faria; max-age=" + 60 * 1000; //1 min
+  });
+  decline.addEventListener("click", function () {
+    document.cookie = "CookieSetBy=false; max-age=" + 60 * 1000; //1 min
 
-//     cookeDiv.classList.remove("show");
-//     cookeDiv.classList.add("hide");
-//   });
-//   let checkCookie = document.cookie.indexOf("CookieSetBy=Faria");
-//   if (checkCookie != -1) {
-//     cookeDiv.classList.remove("show");
-//     cookeDiv.classList.add("hide");
-//   } else {
-//     cookeDiv.classList.remove("hide");
-//   }
-// });
+    cookeDiv.classList.remove("show");
+    cookeDiv.classList.add("hide");
+  });
+  let checkCookie = document.cookie.indexOf("CookieSetBy=Faria");
+  if (checkCookie != -1) {
+    cookeDiv.classList.remove("show");
+    cookeDiv.classList.add("hide");
+  } else {
+    cookeDiv.classList.remove("hide");
+  }
+});
 
 const main = document.querySelector(".card-container ");
 
