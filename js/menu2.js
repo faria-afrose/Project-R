75633e1dc0245c4fc32cfe86639305833b8a1847
Faria Afrose <85243808+faria-afrose@users.noreply.github.com>
2023-07-09 22:02:21 +0600

    Update and rename data.js to menu2.js

--- js/menu2.js
+++ js/menu2.js
@@ -132,3 +132,94 @@ const data2 = [
     para: "3RM 24.99",
   },
 ];
+
+window.addEventListener("DOMContentLoaded", function () {
+  const main1 = document.querySelector(".card-container ");
+  console.log(main1);
+  // card for menu(first) starts
+  data1.forEach((element) => {
+    const card1 = document.createElement("div");
+    card1.classList = " div-x col-lg-4  col-md-6 col-sm-12 mb-5 m-lg-0  ";
+
+    const makeHtml = ` 
+
+<div class='card'>
+<div class='card-id'>
+  <img  class= 'card-img-top' src= ${element.image} alt="t-img">
+
+<div class="card-body">
+
+   <h3 class="card-title">${element.title}</h3>
+  <p class="card-text fw-bold " style="font-size: larger;">${element.para}</p>
+
+ </div>
+</div>
+</div>
+`;
+
+    card1.innerHTML += makeHtml;
+    main1.appendChild(card1);
+
+    card1.addEventListener("click", () => {
+      console.log(`You clicked on the third card (${element.title})`);
+
+      window.location.href = element.href;
+    });
+  });
+  // card for menu(first) ends
+
+  //(cards )on click button starts
+
+  const main2 = document.querySelector(".card-container ");
+  const btn1 = document.querySelector(".btn-class");
+  const btn2 = document.querySelector("#btn2");
+  btn2.addEventListener("click", function (e) {
+    window.location.href = "";
+  });
+  let c = 0;
+  btn1.addEventListener("click", function (e) {
+    if ((e.target.id = "btn")) {
+      c++;
+
+      if (c === 1) {
+        main2.innerHTML = "";
+        btn1.style.backgroundColor = "#eb401eca";
+
+        data2.forEach((element, i) => {
+          const card2 = document.createElement("div");
+          card2.classList = "div-x col-lg-4  col-md-6 col-sm-12 mb-5 m-lg-0";
+
+          const makeHtml = `
+
+  <div class='card'>
+  <div class='card-id2'>
+    <img  class= 'card-img-top' src= ${element.image} alt="t-img">
+
+  <div class="card-body">
+
+     <h3 class="card-title">${element.title}</h3>
+    <p class="card-text fw-bold " style="font-size: larger;">${element.para}</p>
+
+   </div>
+  </div>
+  </div>
+  `;
+
+          card2.innerHTML += makeHtml;
+
+          main2.appendChild(card2);
+
+          card2.addEventListener("click", () => {
+            console.log(`You clicked on the third card (${element.title})`);
+
+            window.location.href = element.href;
+          });
+        });
+      } else if (c === 2) {
+        window.location.href = "";
+
+        c = 0;
+      }
+    }
+  });
+});
